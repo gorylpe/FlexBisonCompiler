2e9f2c44c8da41f7fc380e673ad3ae813b837ac3
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2018-01-20 19:29:08 +0100

    biggest array to declaration start

--- MemoryManager.h
+++ MemoryManager.h
@@ -44,19 +44,32 @@ public:
     }
 
     Variable* getVariable(const string &pid){
-        if (this->variables.count(pid) == 0){
-            if(this->stack.empty())
-                return nullptr;
-            else{
+        if(isVariableDeclared(pid)){
+            if (this->variables.count(pid) > 0){
+                return this->variables[pid];
+            } else {
                 for(auto var : this->stack){
                     if(var->pid == pid){
                         return var;
                     }
                 }
-                return nullptr;
             }
         }
-        return this->variables[pid];
+        return nullptr;
+    }
+
+    bool isVariableDeclared(const string& pid){
+        if(this->variables.count(pid) > 0){
+            return true;
+        }
+
+        for(auto var : this->stack){
+            if(var->pid == pid){
+                return true;
+            }
+        }
+
+        return false;
     }
 
     Variable* pushTempNamedVariable(const string& name, bool modifiable){
@@ -85,5 +98,64 @@ public:
         delete var;
         this->memoryPtr--;
         this->stack.pop_back();
+    }
+
+    string memoryToString(){
+        stringstream ss;
+
+        for(auto& entry : variables){
+            Variable* var = entry.second;
+
+            ss << var->pid << "   ";
+            ss << var->memoryPtr;
+            if(var->type == Variable::Type::PIDNUM){
+                ss << "-" << var->memoryPtr + var->size - 1;
+            }
+            ss << endl;
+        }
+
+        return ss.str();
+    }
+
+    void optimize(){
+        optimizeArraysMemoryPointers();
+    }
+
+    void optimizeArraysMemoryPointers(){
+        cerr << "---BIGGEST ARRAY MOVING TO MEMORY START OPTIMIZATION---" << endl;
+        cerr << memoryToString();
+
+        string biggestArrayPid;
+        cl_I biggestArraySize = 0;
+        //biggest array optimization, move it to start of memory addresses so no need to create indirect addresses
+        for(auto& entry : variables){
+            Variable* var = entry.second;
+            if(var->type == Variable::Type::PIDNUM){
+                if(var->size > biggestArraySize){
+                    biggestArraySize = var->size;
+                    biggestArrayPid = var->pid;
+                }
+            }
+        }
+
+        //found
+        if(!biggestArrayPid.empty()){
+            const cl_I size = variables[biggestArrayPid]->size;
+            const cl_I biggestMemoryPtr = variables[biggestArrayPid]->memoryPtr;
+
+            cerr << "Moving array " << biggestArrayPid << "[" << size << "] to memory start" << endl;
+
+            for(auto& entry : variables){
+                Variable* var = entry.second;
+                if(var->pid != biggestArrayPid){
+                    if(var->memoryPtr < biggestMemoryPtr)
+                        var->memoryPtr += size;
+                }
+            }
+
+            variables[biggestArrayPid]->memoryPtr = 0;
+        }
+        cerr << memoryToString();
+        cerr << "---OPTIMIZATION END---" << endl;
     }
 };
